87fd2e6974ccd02d19df175534252f35046e049e
Israel <israel.nunes74@gmail.com>
2023-09-23 22:15:30 -0300

    product half function

--- src/router/product.router.js
+++ src/router/product.router.js
@@ -5,5 +5,7 @@ const product = require("../controller/product.controller");
 router.post('/product', product.createProduct);
 router.get('/product', product.getAllProduct);
 router.get('/produc/:id', product.getProductByID);
+router.put('/product/:id', product.updateProduct);
+router.delete('/product/:id', product.deleteProduct);
 
 module.exports = router;
