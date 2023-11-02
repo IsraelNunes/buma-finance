7ac83fa8ce68dff6c63b4bc8b6385bef65b6e635
Israel <israel.nunes74@gmail.com>
2023-11-01 23:48:59 -0300

    expanses and revenues working

--- src/router/expanse.router.js
+++ src/router/expanse.router.js
@@ -4,8 +4,12 @@ const expanse = require("../controller/expanse.controller");
 
 router.post('/expanse', expanse.createExpanse);
 router.get('/expanse', expanse.findAllExpanses);
-router.get('expanse/:id', expanse.findOneExpanse);
-router.put('expanse/:id', expanse.updateExpanse);
-router.delete('/expanse', expanse.deleteExpanse);
+router.get('/expanse/:id', expanse.findOneExpanse);
+router.put('/expanse/:id', expanse.updateExpanse);
+router.delete('/expanse/:id', expanse.deleteExpanse);
 
+
+//installments
+router.put('/expanse_installment/:id', expanse.updateInstallment);
+router.delete('/expanse_installmente/:id', expanse.deleteInstallment);
 module.exports = router;
