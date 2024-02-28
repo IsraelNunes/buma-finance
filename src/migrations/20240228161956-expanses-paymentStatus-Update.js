398e9cdc7a9b87ce838af1ba4b2f34881f193646
israel <israel.nunes74@gmail.com>
2024-02-28 13:29:51 -0300

    migrations of expanses and revenues changes in status_payment, now they are enum type

--- src/migrations/20240228161956-expanses-paymentStatus-Update.js
+++ src/migrations/20240228161956-expanses-paymentStatus-Update.js
@@ -3,13 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('Installments', 'status', {
+    await queryInterface.changeColumn('Expanses', 'payment_status', {
       type: Sequelize.ENUM('open', 'paid', 'overdue'),
       defaultValue: "open"
     });
   },
 
   async down (queryInterface, Sequelize) {
-    return Promise.all([queryInterface.changeColumn('Installments', 'status')])
+    return Promise.all([queryInterface.changeColumn('Expanses', 'payment_status')])
   }
 };
